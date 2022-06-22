fa446d529ea1a9d62686f9dfe2a894fbfa3206b1
Viktor <vv_volkov@mail.ru>
2022-06-22 15:30:01 +0300

    feat: port db implementations

---
+++ src/Engine/Impl/Db/EntityManager/DbEntityManagerFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Jabe\Engine\Impl\Db\EntityManager;
+
+use Jabe\Engine\Impl\Cfg\IdGeneratorInterface;
+use Jabe\Engine\Impl\Context\Context;
+use Jabe\Engine\Impl\Db\PersistenceSessionInterface;
+use Jabe\Engine\Impl\Interceptor\SessionFactoryInterface;
+
+class DbEntityManagerFactory implements SessionFactoryInterface
+{
+    protected $idGenerator;
+
+    public function __construct(IdGeneratorInterface $idGenerator)
+    {
+        $this->idGenerator = $idGenerator;
+    }
+
+    public function getSessionType(): string
+    {
+        return DbEntityManager::class;
+    }
+
+    public function openSession(): DbEntityManager
+    {
+        $persistenceSession = Context::getCommandContext()->getSession(PersistenceSessionInterface::class);
+        return new DbEntityManager($idGenerator, $persistenceSession);
+    }
+}
---
+++ src/Engine/Impl/Db/EntityManager/OptimisticLockingResult.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Jabe\Engine\Impl\Db\EntityManager;
+
+class OptimisticLockingResult
+{
+    public const IGNORE = "ignore";
+
+    public const THROW = "throw";
+}
